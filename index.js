6ab5be6c864218ce0df750f4d505f3ed0af86d0d
Luke-6723 <luke@ichigo.uk>
2022-12-18 12:27:20 +0000

    Finish fetching updates from warthunders website

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { default: puppeteer } = require("puppeteer");
-
+const { writeFile } = require("fs/promises")
+const puppeteer = require("puppeteer")
+console.time('Warthunder News Fetcher')
 /**
  * Dev Blogs: https://warthunder.com/en/news/?tags=Development
  * Events: https://warthunder.com/en/news/?tags=Event 
@@ -11,19 +12,92 @@ const urls = {
   changelogs: 'https://warthunder.com/en/game/changelog/'
 };
 
+const widgetSelector = '.showcase__item'
+
+const updates = {
+  devblogs: {},
+  events: {},
+  changelogs: {}
+}
+
+/**
+ * @param {'devblogs' | 'events' | 'changelogs'} type 
+ */
+function handlePosts (type, page) {
+  return new Promise(async (resolve, reject) => {
+    const posts = (await page.$$(widgetSelector)).splice(0, 3)
+    posts.forEach(async (p, index) => {
+      // Create devblog id if it doesnt exist already
+      if(!updates[type][index]) updates[type][index] = {}
+      // Get link
+      const linkElement = await p.$('.widget__link')
+      updates[type][index].link = await (await linkElement.getProperty('href')).jsonValue()
+      // Get poster image
+      const posterElement = await p.$('.widget__poster img')
+      updates[type][index].banner = await (await posterElement.getProperty('src')).jsonValue()
+      // Get title
+      const titleElement = await p.$('.widget__content .widget__title')
+      updates[type][index].title = await (await titleElement.getProperty('textContent')).jsonValue()
+      // Santitize title
+      updates[type][index].title = updates[type][index].title.replace(/\\n/g, '').trim()
+
+      if(index === 2) {
+        resolve()
+      }
+    })
+  })
+}
+
 (async () => {
   // Start browser
   const browser = await puppeteer.launch({
     headless: false
   })
-  // Get pages
-  const pages = await browser.pages()
   // Load Devblogs
-  const page = pages[0]
-  
+  const page = await browser.newPage()
+
+  await page.setJavaScriptEnabled(true)
+  await page.setViewport({
+    width: 1200,
+    height: 700,
+    deviceScaleFactor: 1,
+  })
+
   /**
    * Development Blogs
   */
- page.goto(urls.devblogs)
- await page.waitForSelector('.section')
+
+ await page.goto(urls.devblogs)
+ await page.waitForSelector(widgetSelector, {
+   timeout: 5000
+ })
+
+ await handlePosts('devblogs', page)
+
+ /**
+   * Changelogs
+  */
+
+ await page.goto(urls.changelogs)
+ await page.waitForSelector(widgetSelector, {
+   timeout: 5000
+ })
+
+ await handlePosts('changelogs', page)
+
+ /**
+  * Events
+  */
+
+ await page.goto(urls.events)
+ await page.waitForSelector(widgetSelector, {
+   timeout: 5000
+ })
+
+ await handlePosts('events', page)
+
+ writeFile('./warthunder.json', JSON.stringify(updates, null, 2))
+ browser.close()
+ console.timeEnd('Warthunder News Fetcher')
+
 })()
